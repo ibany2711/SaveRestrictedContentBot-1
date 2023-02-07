a6cbb381704247a70a9729eda0bd1f814f1d2daa
ZaenXP <121328939+Zaaen@users.noreply.github.com>
2023-02-07 13:31:07 +0700

    Create ping.py
    .

---
+++ main/plugins/ping.py
@@ -0,0 +1,31 @@
+
+import asyncio
+from datetime import datetime
+from time import time
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ("week", 60 * 60 * 24 * 7),
+    ("day", 60**2 * 24),
+    ("hour", 60**2),
+    ("min", 60),
+    ("sec", 1),
+)
+
+
+
+@Drone.on(events.NewMessage(incoming=True, pattern="/ping"))
+async def ping_pong(client, m: Message):
+    start = time()
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    m_reply = await m.reply_text("Pinging...")
+    delta_ping = time() - start
+    await m_reply.edit_text(
+        "<b>PONG!!</b>🏓 \n"
+        f"<b>• Pinger -</b> <code>{delta_ping * 1000:.3f}ms</code>\n"
+        f"<b>• Uptime -</b> <code>{uptime}</code>\n"
+    )
